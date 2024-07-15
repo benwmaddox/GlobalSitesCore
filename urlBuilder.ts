1d79e8c8abba4e069f572c5d4948ebe3a8a20219
Ben Maddox <benwmaddox@gmail.com>
2024-07-15 07:38:10 -0400

    Updating url builder and openai translation text

--- urlBuilder.ts
+++ urlBuilder.ts
@@ -3,7 +3,8 @@ import { languageSettings } from "./languages";
 export function urlBuilder(
   directoryEnglish: string | undefined,
   pageEnglish: string | undefined,
-  pagePrefix?: string | undefined
+  pagePrefix?: string | undefined,
+  count?: number
 ): string {
   return `${
     i18next.t("lang", { ns: "meta" }) == languageSettings.defaultLanguage
@@ -11,5 +12,7 @@ export function urlBuilder(
       : i18next.t("lang", { ns: "meta" }) + "/"
   }${directoryEnglish ? i18next.t(directoryEnglish, { ns: "url" }) + "/" : ""}${
     pagePrefix || ""
-  }${pageEnglish ? i18next.t(pageEnglish, { ns: "url" }) + "/" : ""}`;
+  }${
+    pageEnglish ? i18next.t(pageEnglish, { ns: "url", count: count }) + "/" : ""
+  }`;
 }
