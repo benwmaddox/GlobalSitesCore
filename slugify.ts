ce50383c6f2eb6c6d6af2473976580c82ed52e8a
Ben Maddox <benwmaddox@gmail.com>
2024-08-21 10:14:29 -0400

    More slugify updates

--- slugify.ts
+++ slugify.ts
@@ -1,21 +1,28 @@
 import { titleCase } from './titleCase';
 
 export function slugifyText(input: string): string {
-	return titleCase(input)
-		.replace(/ /g, '-')
-		.replace(/_/g, '-')
-		.replace(/,/g, '-')
-		.replace(/\(/g, '-')
-		.replace(/\)/g, '-')
-		.replace(/--/g, '-')
-		.replace(/&/g, 'and')
-		.replace(/'/g, '')
-		.replace(/:/g, '')
-		.replace(/;/g, '')
-		.replace(/"/g, '')
-		.replace(/\./g, '')
-		.replace(/!/g, '')
-		.replace(/\?/g, '')
-		.replace(/%/g, '')
-		.replace(/--/g, '-');
+	return (
+		titleCase(input)
+			.replace(/ /g, '-')
+			.replace(/_/g, '-')
+			.replace(/,/g, '-')
+			.replace(/\(/g, '-')
+			.replace(/\)/g, '-')
+			.replace(/--/g, '-')
+			.replace(/&/g, 'and')
+			.replace(/'/g, '')
+			.replace(/:/g, '')
+			.replace(/;/g, '')
+			.replace(/"/g, '')
+			.replace(/\./g, '')
+			.replace(/!/g, '')
+			.replace(/\?/g, '')
+			.replace(/%/g, '')
+			.replace(/¿/g, '')
+			.replace(/--/g, '-')
+			//replace starting and trailing hyphens
+			.replace(/^-/, '')
+			.replace(/-$/, '')
+			.toLowerCase()
+	);
 }
