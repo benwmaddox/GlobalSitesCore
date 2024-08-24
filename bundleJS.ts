b73f374776d0c909816f6dc8dfe39927b99ec0f6
Ben Maddox <benwmaddox@gmail.com>
2024-08-23 22:12:36 -0400

    Adding in translations

--- bundleJS.ts
+++ bundleJS.ts
@@ -7,6 +7,8 @@ import { glob } from 'glob';
 import { ellipsis } from './ConsoleText';
 import { rollup } from 'rollup';
 import { inlineTranslations } from './inlineTranslations';
+import { languageSettings } from './languages';
+import i18next from './i18n';
 
 export async function bundleJSFiles(globPattern: string, minify: boolean): Promise<FileResult[]> {
 	const pageFiles = glob.sync(globPattern, { withFileTypes: false });
@@ -17,49 +19,52 @@ export async function bundleJSFiles(globPattern: string, minify: boolean): Promi
 	// inline translations are used
 
 	for (const file of pageFiles) {
-		try {
-			const bundle = await rollup({
-				input: file,
-				plugins: [
-					inlineTranslations(),
-					json(),
-					nodeResolve({
-						mainFields: ['browser'],
-						preferBuiltins: true
-					}),
-					commonjs(),
-					...(minify ? [terser()] : [])
-				]
-			});
+		for (const lang of languageSettings.languages) {
+			await i18next.changeLanguage(lang);
+			try {
+				const bundle = await rollup({
+					input: file,
+					plugins: [
+						inlineTranslations(),
+						json(),
+						nodeResolve({
+							mainFields: ['browser'],
+							preferBuiltins: true
+						}),
+						commonjs(),
+						...(minify ? [terser()] : [])
+					]
+				});
 
-			const { output } = await bundle.generate({
-				format: 'iife',
-				name: 'page'
-			});
+				const { output } = await bundle.generate({
+					format: 'iife',
+					name: 'page'
+				});
 
-			const content = output[0].code;
-			const relativePath = file
-				.replace('build/', '')
-				.replace('build\\', '')
-				.replace('.js', minify ? '.e.js' : '.ee.js');
+				const content = output[0].code;
+				const relativePath = file
+					.replace('build/', '')
+					.replace('build\\', '')
+					.replace('.js', minify ? `.${lang}.min.js` : `.${lang}.js`);
 
-			results.push({
-				relativePath,
-				content,
-				includeInSitemap: false
-			});
+				results.push({
+					relativePath,
+					content,
+					includeInSitemap: false
+				});
 
-			const translationMatches = content.match(/translate\((.*?)\)/g);
+				const translationMatches = content.match(/translate\((.*?)\)/g);
 
-			if (translationMatches) {
-				//const translationKeys = translationMatches.map((match) => match.replace(/i18next\.t\((.*?)\)/, '$1'));
-				//console.log(`Found ${translationKeys.length} translation keys in ${file}`);
-				console.log({ translationMatches });
+				if (translationMatches) {
+					//const translationKeys = translationMatches.map((match) => match.replace(/i18next\.t\((.*?)\)/, '$1'));
+					//console.log(`Found ${translationKeys.length} translation keys in ${file}`);
+					console.log({ translationMatches });
+				}
+
+				await bundle.close();
+			} catch (error) {
+				console.error(`Error processing file ${file}:`, error);
 			}
-
-			await bundle.close();
-		} catch (error) {
-			console.error(`Error processing file ${file}:`, error);
 		}
 	}
 
