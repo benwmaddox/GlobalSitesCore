947f507551096c62f407c2f18ece46e01e2768b1
Ben Maddox <benwmaddox@gmail.com>
2024-09-06 09:51:08 -0400

    Fixed watch dev to show output

--- watchDev.ts
+++ watchDev.ts
@@ -9,34 +9,21 @@ let commandRunning = false;
 
 // Function to execute a command
 const executeCommand = async (command: string): Promise<void> => {
-  const commandStartTime = new Date().getTime();
   try {
-    const childProcess = exec(command);
-
-    childProcess.stdout?.on("data", (data) => {
-      process.stdout.write(data);
-    });
-
-    childProcess.stderr?.on("data", (data) => {
-      process.stderr.write(data);
-    });
-
-    await new Promise<void>((resolve, reject) => {
-      childProcess.on("close", (code) => {
-        if (code === 0) {
-          resolve();
-        } else {
-          reject(new Error(`Command exited with code ${code}`));
-        }
-      });
-    });
+    const { stdout, stderr } = await execPromise(command);
+    console.log(stdout);
+    if (stderr) {
+      console.error(stderr);
+      // run once more, just in case.
+      await execPromise(command);
+      console.log(stdout);
+      if (stderr) {
+        console.error(stderr);
+      }
+    }
   } catch (error) {
     console.error("Error executing command:", error);
   }
-  const commandEndTime = new Date().getTime();
-  console.log(
-    `TypeScript + Build took ${commandEndTime - commandStartTime}ms total.`
-  );
 };
 
 // Function to handle changes
@@ -71,5 +58,3 @@ watcher.on("all", (event, path) => {
   console.log(`File ${path} has been ${event}`);
   setTimeout(handleChange, 0);
 });
-
-setTimeout(handleChange, 0);
